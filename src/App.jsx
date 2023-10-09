cc0373cd7b4ad7cc69cd019c4310b7a4ae99a488
brutumfulmen97 <vlatkooop@gmail.com>
2023-10-09 20:53:43 +0200

    tastatura ubacena

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,40 @@ function App() {
     const [word, setWord] = useState("WORDLE");
     const [board, setBoard] = useState(Array(6).fill(null));
     const [currentGuess, setCurrentGuess] = useState("");
+    const [keyboard] = useState([
+        ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+        ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+        ["enter", "z", "x", "c", "v", "b", "n", "m", "backspace"],
+    ]);
+
+    function handleType(key) {
+        if (key === "enter") {
+            if (currentGuess.length !== 5) return;
+            setBoard((prev) => {
+                const newBoard = [...prev];
+                newBoard[newBoard.findIndex((r) => r === null)] = currentGuess;
+                return newBoard;
+            });
+            setCurrentGuess("");
+            return;
+        }
+        if (key === "backspace") {
+            setCurrentGuess((prev) => prev.slice(0, -1));
+            return;
+        }
+        if (key.length > 1) {
+            return;
+        }
+        if (currentGuess.length === 5) {
+            return;
+        }
+        setCurrentGuess((prev) => prev + key);
+    }
 
     useEffect(() => {
         function handleType(e) {
             if (e.key === "Enter") {
+                if (currentGuess.length !== 5) return;
                 setBoard((prev) => {
                     const newBoard = [...prev];
                     newBoard[newBoard.findIndex((r) => r === null)] =
@@ -38,7 +68,7 @@ function App() {
     return (
         <main className="w-full h-screen flex flex-col justify-center items-center gap-24">
             <h1 className="text-2xl font-bold">{word}</h1>
-            <div className="bg-teal-900 w-[500px] h-[600px] flex flex-col justify-between gap-4 p-4">
+            <div className="bg-transparent w-[500px] h-[600px] flex flex-col justify-between gap-4 p-4">
                 {board.map((row, i) => {
                     const isCurrentGuess =
                         i === board.findIndex((r) => r === null);
@@ -48,6 +78,26 @@ function App() {
                             key={i}
                             isCurrentGuess={isCurrentGuess}
                         />
+                    );
+                })}
+            </div>
+            <div>
+                {keyboard.map((row, i) => {
+                    return (
+                        <div
+                            key={i}
+                            className="flex justify-center items-center gap-2"
+                        >
+                            {row.map((key, keyI) => (
+                                <div
+                                    key={keyI}
+                                    className="p-4 bg-slate-200 mt-2 text-black text-center rounded-md cursor-pointer hover:bg-slate-400"
+                                    onClick={() => handleType(key)}
+                                >
+                                    {key}
+                                </div>
+                            ))}
+                        </div>
                     );
                 })}
             </div>
@@ -65,7 +115,7 @@ function Line({ row, isCurrentGuess }) {
         tiles.push(
             <div
                 key={i}
-                className="w-full h-full bg-red-300 flex justify-center items-center text-2xl text-black font-bold"
+                className="w-full h-full bg-white flex justify-center items-center text-2xl text-black font-bold "
             >
                 {char}
             </div>
